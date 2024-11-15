f0f991ddeaf7e3a514e41b38dc6e7fe8e1fcb0c4
Dieu Huynh <dieu_huynh@apple.com>
2024-11-15 15:57:02 -0800

    move Loading page to components folder

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense, forwardRef, useState } from "react";
 import lowerLevelImage from "../../assets/Warriors section 109.png";
-import Loading from "../Loading/Loading";
+import Loading from "../../components/Loading/Loading";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import ImageModal from "../Modals/ImageModal";
